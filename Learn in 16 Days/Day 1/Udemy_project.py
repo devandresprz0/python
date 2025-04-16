48f0adca221644e1332c898f747bf2f2c3db4b5f
devandresprz0 <dev.andresprz@gmail.com>
2025-04-16 12:24:38 -0500

    Add files via upload

---
+++ Learn in 16 Days/Day 1/Udemy_project.py
@@ -0,0 +1,5 @@
+#Fabrica de cervezas, le falta una identidad, un nombre
+#Programa de dos preguntas y cual es el nombre de tu cerveza
+#Salida esperada 'La cerveza se llama " "' 2 lineas
+
+print('The beer call\n'+'\''+input('What is your favorite candy?: '),input('What is your dogs name?: ')+'\'')
---
+++ Learn in 16 Days/Day 1/Udemy_string.py
@@ -0,0 +1,18 @@
+print('100 + 50')
+print('Hola' + 'Andres')
+print('Hola','\'Andres\'')
+print('Line 1\nLine 2')
+print('Lista de super')
+print('\tTomate')
+print('This isn\'t a number')
+print('Slash moved \\')
+
+#practice 1
+print('Línea 1\nLínea 2\nLínea 3')
+
+#practice 2
+print('A\tB\tC\nD\tE\tF\nG\tH\tI')
+
+#practice 3
+print('Barra Normal: /')
+print('Barra Invertida: \\')
